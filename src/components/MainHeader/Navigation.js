85f7322f12c65b1bafbbaccb68fa0983285f6e22
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-05-16 06:16:01 +0530

    Context API used by consumer property

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,28 +1,36 @@
 import React from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContext from '../../storeContext/authContext';
 
 const Navigation = (props) => {
   return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
+    <AuthContext.Consumer>
+      {(cxt) =>{
+        return (
+          <nav className={classes.nav}>
+            <ul>
+            {cxt.isLoggedIn && (
+            <li>
+              <a href="/">Users</a>
+            </li>
+            )}
+            {cxt.isLoggedIn && (
+            <li>
             <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
+            </li>
+            )}
+            {cxt.isLoggedIn && (
+            <li>
             <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+            </li>
+            )}
+            </ul>
+          </nav>
+        )
+      }}
+    
+    </AuthContext.Consumer>
   );
 };
 
